7214861626a3049c45380a459f49b76c1a5db555
Melyssa Moya <moyamelyssa@gmail.com>
2023-10-05 19:42:48 +0000

    feat: Implementação do destema de médias de alunos

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// Lógica da Programação
-// Problema: Obter 3 notas de um aluno, calcular a média;
-// Exibir a média e exibir a situação do alno: aprovado, reprovado;
+function mediaAluno() {
 
-console.log('Início do programa')
+    console.log('Início do programa');
 
-// Declaração de variáveis
-const n1 = parseInt(prompt("Digite a primeira nota:"));
-const n2 = parseInt(prompt("Digite a segunda nota:"));
-const n3 = parseInt(prompt("Digite a terceira nota:"));
+    // Declaração de variáveis
+    const n1 = parseInt(document.getElementById('n1').value);
+    const n2 = parseInt(document.getElementById('n2').value);
+    const n3 = parseInt(document.getElementById('n3').value);
 
-// Cálculo da média
-media = (n1 + n2 + n3) / 3;
+    // Cálculo da média
+    media = (n1 + n2 + n3) / 3;
 
-console.log("A 1° nota é:", n1);
-console.log("A 2° nota é:", n2);
-console.log("A 3° nota é:", n3);
+    document.getElementById('resultado').innerHTML = `A média do aluno é: ${media.toFixed(2)}`
 
-console.log("A média do aluno é: " + media);   
+    if(media < 7 && media >= 5){
+        document.getElementById('resultado').innerHTML += "<br> E sua situação é: EXAME";
+    } else if(media < 5){
+        document.getElementById('resultado').innerHTML += "<br> E sua situação é: REPROVADO";
+    } else {
+        document.getElementById('resultado').innerHTML += "<br> E sua situação é: APROVADO";
+    }
 
-if(media >= 5){
-    window.alert("Você foi aprovado!")
-} else {
-    window.alert("Você foi reprovado")
+
+    console.log("Fim do programa.");
 }
-
-console.log("Fim do programa.")
